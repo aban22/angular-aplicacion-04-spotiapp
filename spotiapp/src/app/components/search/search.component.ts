17621cbab2afe929d06043d2be2234f4a67abab0
Abel Fernandez <abelfergodevelop@gmail.com>
2019-02-13 19:56:01 +0100

    Clase 95

--- spotiapp/src/app/components/search/search.component.ts
+++ spotiapp/src/app/components/search/search.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
+
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styles: []
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
 
-  constructor() { }
+    artistas: any[] = [];
 
-  ngOnInit() {
-  }
+    constructor(private spotifyService: SpotifyService) { }
+
+    buscar(termino: string) {
+        console.log(termino);
+        this.spotifyService.getArtista(termino).subscribe(
+            (response: any) => {
+                console.log(response.artists.items);
+                this.artistas = response.artists.items;
+            }
+        );
+     }
 
 }
--- spotiapp/src/app/services/spotify.service.ts
+++ spotiapp/src/app/services/spotify.service.ts
@@ -12,11 +12,18 @@ export class SpotifyService {
 
 
     getNewReleases() {
-
         const headers = new HttpHeaders({
             Authorization: 'Bearer BQAWA1cBlfNcH2tz3SS5IvitiAdvz-foTgI81O1lrh0TP6Dv_UjnMj-QOSsdfrQYtziACzvUyLb6JjTLaSo'
         });
 
         return this.http.get('https://api.spotify.com/v1/browse/new-releases', {headers});
+    }
+
+    getArtista(termino: string) {
+        const headers = new HttpHeaders({
+            Authorization: 'Bearer BQAWA1cBlfNcH2tz3SS5IvitiAdvz-foTgI81O1lrh0TP6Dv_UjnMj-QOSsdfrQYtziACzvUyLb6JjTLaSo'
+        });
+
+        return this.http.get(`https://api.spotify.com/v1/search?q=${termino}=&type=artist&limit=15`, {headers});
     }
 }
